3ea3dfa95554f7e6565ff489af2dba343ac75ff1
Patrick Drechsler <patrick@pdrechsler.de>
2021-01-20 15:40:45 +0100

    first failing test

--- OrderTakingEvolved/OrderTakingEvolved.Tests/PlaceOrder.Api.Tests.fs
+++ OrderTakingEvolved/OrderTakingEvolved.Tests/PlaceOrder.Api.Tests.fs
@@ -1,21 +1,66 @@
 module PlaceOrder.Api.Tests
 
-open System
 open Xunit
 open Swensen.Unquote
+open Newtonsoft.Json
+open OrderTaking.PlaceOrder
 open OrderTaking.PlaceOrder.Api
+
+let createOrderFormDto : OrderFormDto =
+    
+    let orderId = "1"
+    
+    let customerInfoDto = {
+        CustomerInfoDto.FirstName = "Homer"
+        LastName = "Simpson"
+        EmailAddress = "homer.simpson@aol.com"
+        VipStatus = "unknown"
+    }
+    
+    let addressDto = {
+        AddressDto.AddressLine1 = "Evergreen Terrace 1"
+        AddressLine2 = ""
+        AddressLine3 = ""
+        AddressLine4 = ""
+        City = "Springfield"
+        ZipCode = "12345"
+        State = "Some State"
+        Country = "USA"
+    }
+    
+    let orderFormLineDto = {
+        OrderFormLineDto.OrderLineId = "1"
+        ProductCode = "abc"
+        Quantity = 1m
+    }
+    
+    let orderFormLineDtos = [orderFormLineDto]
+    
+    let promotionCode = "promoCode123"
+    
+    let orderFormDto = {
+        OrderFormDto.OrderId = orderId
+        CustomerInfo = customerInfoDto
+        ShippingAddress = addressDto
+        BillingAddress = addressDto
+        Lines = orderFormLineDtos
+        PromotionCode = promotionCode
+    }
+    
+    orderFormDto
+
+let toJson orderFormDto = JsonConvert.SerializeObject(orderFormDto)
 
 [<Fact>]
 let ``Happy case`` () =
-    
-    
-    let body = ""
+
+    let body = createOrderFormDto |> toJson
+
     let request = {
         Action = ""
         Uri = ""
         Body = body
     }
-    
     
     let (result: HttpResponse) =
         async {
